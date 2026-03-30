633a0217aa0b27b0237477863ba56162480769fc
Seval Karaosmanoğlu <sevallacin@gmail.com>
2026-03-30 11:27:20 +0300

    Implement start() to handle FLASK_ENV with correct messages

--- flask_option.py
+++ flask_option.py
@@ -4,7 +4,14 @@ import os
 
 def start():
     """returns the right message"""
-    pass  # YOUR CODE HERE
+    flask_env = os.getenv("FLASK_ENV")
+
+    if flask_env == "production":
+        return "Starting in production mode..."
+    elif flask_env == "development":
+        return "Starting in development mode..."
+    else:
+        return "Starting in empty mode..."
 
 if __name__ == "__main__":
     print(start())
